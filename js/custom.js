f6a3897f5b56f4e591e258716253476591e54bb2
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-12-19 21:18:37 +0100

    Add more pics and text

--- js/custom.js
+++ js/custom.js
@@ -33,9 +33,9 @@ $(document).ready(function() {
   $(function() {
     $('body').vegas({
         slides: [
-            { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
+            { src: 'images/slide-1.jpg' },
+            { src: 'images/slide-2.jpg' },
+            { src: 'images/slide-3.jpg' }
         ],
         timer: false,
         transition: [ 'zoomIn', ],
